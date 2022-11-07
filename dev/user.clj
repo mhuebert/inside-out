da1dca2ea07c278cb513e0c947130630608f9b5a
Matthew Huebert <mhuebert@gmail.com>
2022-11-07 14:20:05 +0100

    clerk version

--- dev/user.clj
+++ dev/user.clj
@@ -1,10 +1,8 @@
 (ns user
   (:require [clojure.string :as str]
             [nextjournal.clerk :as clerk]
-            [nextjournal.clerk.config :as config]
             nextjournal.clerk.viewer
             [nextjournal.clerk.dev-launcher :as launcher]
-            [nextjournal.clerk.viewer.builder :as builder]
             [clojure.java.shell :refer [sh]]))
 
 ;; one-time
@@ -15,25 +13,13 @@
                    :out-path "public"
                    :watch-paths ["dev"]
                    :show-filter-fn #(str/includes? % "notebooks")
-                   :extra-namespaces '[inside-out.sci-config]
-                   }))
-
-(defn publish! [& _]
-  (clerk/build! {:index "dev/inside_out/notebook.clj"
-                 :compile-css true
-                 :bundle? false
-                 :out-path "public/build"
-                 :extra-namespaces '[inside-out.sci-config]}))
+                   :extra-namespaces '[inside-out.sci-config]}))
 
 (comment
 
- (do (shadow.cljs.devtools.api/stop-worker :viewer)
-     (start))
+ (shadow.cljs.devtools.api/stop-worker :viewer)
 
- (publish!)
-
- (require 'nextjournal.clerk.viewer.builder :reload)
- (require 'nextjournal.clerk.builder :reload)
+ (start)
 
  (do
    ;; start a dev server on port 7999
